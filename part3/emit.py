9e2f4d207e13a801e593d819f77067a962890f6d
Gabriel Sroka <gabrielsroka@gmail.com>
2026-02-03 09:46:18 -0800

    Update emit.py

--- part3/emit.py
+++ part3/emit.py
@@ -1,19 +1,24 @@
 # Emitter object keeps track of the generated code and outputs it.
 class Emitter:
-    def __init__(self, fullPath):
+    def __init__(self, fullPath, tab=4):
         self.fullPath = fullPath
-        self.header = ""
-        self.code = ""
+        self.tab = tab
+        self.code = ''
+        self.line = ''
+        self.indent = 0
 
     def emit(self, code):
-        self.code += code
+        if self.line == '':
+            self.line = ' ' * self.indent * self.tab
+        self.line += code
 
     def emitLine(self, code):
-        self.code += code + '\n'
-
-    def headerLine(self, code):
-        self.header += code + '\n'
+        if code.endswith('}'): self.indent -= 1
+        self.emit(code)
+        self.code += self.line + '\n'
+        self.line = ''
+        if code.endswith('{'): self.indent += 1
 
     def writeFile(self):
         with open(self.fullPath, 'w') as outputFile:
-            outputFile.write(self.header + self.code)
+            outputFile.write(self.code)
